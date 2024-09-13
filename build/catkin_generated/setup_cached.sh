1519015328ca93c4c4fa4ee82f2eebd02e25b81f
Star-Catching <1612627719@qq.com>
2024-09-13 09:35:40 +0800

    first commit

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/wxz/note/S-FAST_LIO/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/wxz/note/S-FAST_LIO/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/home/wxz/3dparty/opencv-4.5.1/install_path/lib:/usr/local/openssl-3.0/lib64'
+export PATH='/opt/ros/noetic/bin:/usr/local/openssl-3.0/bin:/home/wxz/anaconda3/bin:/home/wxz/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda/bin'
+export PWD='/home/wxz/note/S-FAST_LIO'
+export ROS_PACKAGE_PATH='/home/wxz/note/S-FAST_LIO:/opt/ros/noetic/share'
